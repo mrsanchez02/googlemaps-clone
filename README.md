d2cb43e1b5f9f4eaa0f3ea374b89c16a648b48fc
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2021-07-11 19:23:59 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# README
+
+Este es un clon de Google Maps, construido utilizando la libreria y API del proveedor de mapas Mapbox.
+[Mapbox Homepage](https://www.mapbox.com/)
+
+Mapbox es una herramienta increíblemente poderosa para crear aplicaciones basadas en mapas, pero puede ser bastante abrumador para empezar. Para este proyecto he usado la API de geolocalización del navegador, así como los complementos de Mapbox.
+
+---
+💡**Nota:**
+
+    Mapbox es un proveedor de mapas en línea realizados por encargo para páginas webs como Foursquare, Pinterest, Evernote, Financial Times, EThe Weather Channel y Uber Tecnologías.​
+[Wikipedia](https://wikipedia.org/wiki/Mapbox)
---
+++ app.js
@@ -0,0 +1,32 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibXJzYW5jaGV6MDIiLCJhIjoiY2txenFzZ2ZxMWt0bTJ2bzhtbDQ4aTlkNSJ9.bddffmVD_boW5kXO-cm_ZQ';
+
+const successLocation = (position) => {
+    setupMap([position.coords.longitude,position.coords.latitude])
+}
+
+const errorLocation = () => {
+    setupMap([-69.893333,18.476389,])
+}
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation,{enableHighAccuracy: true})
+
+const setupMap = (center) => {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15
+        });
+    // Mostrar Controles de navegacion (zoomIn, ZoomOut y Compass)
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav,'top-right')
+    
+    // Mostrar controles de direccion (Para trazar ruta)
+    map.addControl(
+        new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+            }),
+            'top-left'
+    )
+}
+
