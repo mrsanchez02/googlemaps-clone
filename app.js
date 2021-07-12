f1555ac2be25d23b275759f5910eff32843b4e7a
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2021-07-11 21:28:59 -0400

    Se agrega opcion de busqueda

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibXJzYW5jaGV6MDIiLCJhIjoiY2txenFzZ2ZxMWt0bTJ2bzhtbDQ4aTlkNSJ9.bddffmVD_boW5kXO-cm_ZQ';
 
 const successLocation = (position) => {
-    setupMap([position.coords.longitude,position.coords.latitude])
+    setupMap([position.coords.longitude, position.coords.latitude])
 }
 
 const errorLocation = () => {
-    setupMap([-69.893333,18.476389,])
+    setupMap([-69.893333, 18.476389,])
 }
 
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation,{enableHighAccuracy: true})
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true })
 
 const setupMap = (center) => {
     const map = new mapboxgl.Map({
@@ -16,17 +16,27 @@ const setupMap = (center) => {
         style: 'mapbox://styles/mapbox/streets-v11',
         center: center,
         zoom: 15
-        });
+    });
+    
     // Mostrar Controles de navegacion (zoomIn, ZoomOut y Compass)
     const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav,'top-right')
+    map.addControl(nav, 'top-right')
+
+    // Buscador.
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        limit:3,
+        placeholder:'🔍 Search'
+    })
+    map.addControl(geocoder,'bottom-left');
     
     // Mostrar controles de direccion (Para trazar ruta)
-    map.addControl(
+     map.addControl(
         new MapboxDirections({
             accessToken: mapboxgl.accessToken
-            }),
-            'top-left'
+        }),
+        'top-left'
     )
 }
 
